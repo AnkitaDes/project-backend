7df914ce062ff1a97df33105ec0ac95d32510d7b
AnkitaDes <desai_ankita@outlook.com>
2024-02-17 18:45:13 -0500

    added utils

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
-// require("dotenv").config();
 import dotenv from "dotenv";
 import connectDB from "./db/database.js";
 dotenv.config({ path: "./env" });
 
-connectDB();
-
-// async () => {
-//   try {
-//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-//     app.on("error", () => {
-//       console.log("Error connecting to the database:", error);
-//     });
-//   } catch (error) {
-//     console.error("ERROR:", error);
-//     throw error;
-//   }
-// };
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port: ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Mongo db connection failed!!!", error);
+  });
