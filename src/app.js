16633fb8fd15b5ce3ed07c5524e3a1eaeeed1a8e
AnkitaDes <desai_ankita@outlook.com>
2024-02-18 11:43:48 -0500

    add register controller and bug fixes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import { c } from "tar";
 
 const app = express();
 
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,18 +1,18 @@
-const asyncHandler = (requestHandler) => async (req, res, next) => {
-  try {
-    await requestHandler(req, res, next);
-  } catch (error) {
-    res.status(err.code || 500).json({
-      success: false,
-      message: err.message,
-    });
-  }
+// const asyncHandler = (requestHandler) => async (req, res, next) => {
+//   try {
+//     await requestHandler(req, res, next);
+//   } catch (error) {
+//     res.status(err.code || 500).json({
+//       success: false,
+//       message: err.message,
+//     });
+//   }
+// };
+
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
 };
 
 export { asyncHandler };
-
-// const asyncHandler = (requestHandler) => {
-//   (req, res, next) => {
-//     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
-//   };
-// };
